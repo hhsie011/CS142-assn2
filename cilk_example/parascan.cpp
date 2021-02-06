c38ec2f76076084b6facc4f865e8c0b1781e93d3
Hsiang-Yin Hsieh <hhsie011@wch133-42.cs.ucr.edu>
2021-02-06 03:11:46 -0800

    finalized parallel scan implementation

--- cilk_example/parascan.cpp
+++ cilk_example/parascan.cpp
@@ -8,22 +8,17 @@ using namespace std;
 
 int th = 128;
 
-void scan(int* A, int n, int ps, int& leftSum) {
-    // if (n <= th) {
-    //     int ret = 0;
-    //     for (int i = 0; i < n; i++) {
-    //         ret += A[i];
-    //     }
-    //     return ret;
-	// }
-    if (n == 1) {
-        //leftSum = A[0];
-        A[0] = ps;
-	return;
-    }
+void scan(int* A, int n, int& ps, int& leftSum) {
+     if (n <= th) {
+         for (int i = 0; i < n; i++) {
+	     leftSum = A[i];
+             A[i] = ps;
+	     ps += leftSum;
+         }
+	 return;
+     }
     cilk_spawn scan(A, n/2, ps, leftSum);
-    //ps += leftSum;
-    scan(A+n/2, n-n/2, ps + A[A+n/2-1], leftSum);
+    scan(A+n/2, n-n/2, ps, leftSum);
     cilk_sync;
     return;
 }
@@ -44,8 +39,10 @@ int main(int argc, char** argv) {
 	scan(A, n, ps, leftSum);
 	t.stop(); cout << "time: " << t.get_total() << endl;
 	
-	for (int i = 0; i < n; i++) cout << A[i] << " ";
-	cout << endl;
+	//for (int i = 0; i < n; i++) cout << A[i] << " ";
+	//cout << endl;
+
+	cout << A[n-1] << endl;
   
     delete []A;
 	
